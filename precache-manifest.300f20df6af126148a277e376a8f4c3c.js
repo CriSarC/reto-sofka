751e231c27ab902366e9cecaa96f117975c3fbfe
Cristian Gonzalo Sarmiento Castro <sarmientocastrocris@gmail.com>
2021-10-19 11:02:10 -0500

    Updates

---
+++ precache-manifest.300f20df6af126148a277e376a8f4c3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6d9e190dff1d390a8674202742a147",
+    "url": "/reto-sofka/index.html"
+  },
+  {
+    "revision": "606342579249eb92a5d0",
+    "url": "/reto-sofka/static/css/main.8fa9b716.chunk.css"
+  },
+  {
+    "revision": "75a47c4de018a20fb12f",
+    "url": "/reto-sofka/static/js/2.d844cb6b.chunk.js"
+  },
+  {
+    "revision": "74227f154255be9ce828ff77a1b05766",
+    "url": "/reto-sofka/static/js/2.d844cb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606342579249eb92a5d0",
+    "url": "/reto-sofka/static/js/main.05070db1.chunk.js"
+  },
+  {
+    "revision": "5b1da45d03efb5e908cd",
+    "url": "/reto-sofka/static/js/runtime-main.d80b98e0.js"
+  },
+  {
+    "revision": "aa8c631f1c677d1710bc65e9fe648ba0",
+    "url": "/reto-sofka/static/media/pexels-prince-lakra-9186464.aa8c631f.jpg"
+  }
+]);
